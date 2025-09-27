a03477a9bb0b679ea8845b44d7e06238588107d0
WeSummitMountains <jason@wesummitmountains.com>
2025-09-27 01:09:54 -0500

    Update Test

---
+++ .sfdx/typings/lwc/WSDocuments_Logo_Only.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/WSDocuments_Logo_Only" {
+    var WSDocuments_Logo_Only: string;
+    export default WSDocuments_Logo_Only;
+}
---
+++ .sfdx/typings/lwc/WSDocuments_Logo_Only1.asset.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/WSDocuments_Logo_Only1" {
+    var WSDocuments_Logo_Only1: string;
+    export default WSDocuments_Logo_Only1;
+}
